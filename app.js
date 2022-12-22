a7bc842180444be69cfb73f381b8ee39182933a6
nodejshobby <nodejshobby@gmail.com>
2022-12-22 17:02:41 +0100

    Fixing Session

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.use(session({
   secret: process.env.SESSION_SECRET,
   resave: false,
   saveUninitialized: true,
-  cookie: { secure: false }
+  cookie: { secure: false , maxAge: 3600000}
 }))
 
 // Client Ui endpoints

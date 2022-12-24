3cdd734bbd3459b840b89613dd80ed2b38352c7e
Anuoluwapo Omobolaji <francismobolaji1990@gmail.com>
2022-12-24 15:03:23 +0100

    time and date

--- public/js/classes.js
+++ public/js/classes.js
@@ -2,20 +2,20 @@ let dayOfWeek = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
 let monthOfYear = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
 
 $(window).on('load',function(){
-    var class_div = $("#class");
+    let class_div = $("#class");
 
-    var settings = {
+    let settings = {
         "url": "/classes",
         "method": "GET",
       };
       
       $.ajax(settings).done(function (response) {
         if(response.error){
-            var url = "/login";
+            let url = "/login";
             $(location).attr('href',url);     
         }else{
-            var classes = response.results;
-            var dataHtml = `<thead>
+            let classes = response.results;
+            let dataHtml = `<thead>
             <tr>
               <th scope="col">Teacher</th>
               <th scope="col">Description</th>
@@ -48,8 +48,8 @@ $(window).on('load',function(){
                 <td>${cls.description}</td>
                 <td class="p">${dayOfWeek[startDate.getDay()]} ${monthOfYear[startDate.getMonth()]} ${startDate.getDate()} ${startDate.getFullYear()} ${startDate.getHours()>12? startDate.getHours()-12:startDate.getHours()}:${startDate.getMinutes()} ${startDate.getHours() > 11? "PM":"AM"}</td>
                 <td>${dayOfWeek[endDate.getDay()]} ${monthOfYear[endDate.getMonth()]} ${endDate.getDate()} ${endDate.getFullYear()} ${endDate.getHours()>12? endDate.getHours()-12:endDate.getHours()}:${endDate.getMinutes()} ${endDate.getHours() > 12? "PM":"AM"}</td>
-                <td>${cls.end >  getCurrent() ?  "<span class='badge text-success  d-inline'><span class='bg-success mx-1 rounded-circle d-inline-block' style='width: 6px; height: 6px' ></span>Active</span>": "<span class='badge text-danger'><span class='bg-danger mx-1 rounded-circle d-inline-block' style='width: 6px; height: 6px' ></span>Past</span>" }</td>
-                <td><a class="btn btn-sm ${cls.end >  getCurrent() ?  "btn-success": "disabled btn-secondary"}" href="${cls.room_url}" ><i class="fa fa-sign-in"></i> Enter class</a></td>
+                <td>${endDate >  new Date() ?  "<span class='badge text-success  d-inline'><span class='bg-success mx-1 rounded-circle d-inline-block' style='width: 6px; height: 6px' ></span>Active</span>": "<span class='badge text-danger'><span class='bg-danger mx-1 rounded-circle d-inline-block' style='width: 6px; height: 6px' ></span>Past</span>" }</td>
+                <td><a class="btn btn-sm ${endDate >  new Date() ?  "btn-success": "disabled btn-secondary"}" href="${cls.room_url}" ><i class="fa fa-sign-in"></i> Enter class</a></td>
                 <td><a class="btn btn-sm btn-danger" href="/class/delete/${cls.uuid}/"><i class="fa fa-trash"></i> Delete</a></td>
                 <td><a class="btn btn-sm btn-warning" data-bs-toggle="modal" data-bs-target="#${cls.slug}" href="/class/edit/${cls.uuid}/"><i class="fa fa-edit"></i> Edit</a> <div class="modal fade" id="${cls.slug}" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
                 <div class="modal-dialog">
--- public/js/homeclasses.js
+++ public/js/homeclasses.js
@@ -25,8 +25,10 @@ $(window).on('load',()=>{
             </tr>
         </thead> <tbody>`;
             classes.forEach(cls => {
-              let startDate = new Date(cls.start);
-              let endDate = new Date(cls.end);
+              let startServerDate = new Date(cls.start);
+              let startDate = returnLocalTime(startServerDate)
+              let endServerDate = new Date(cls.end)
+              let endDate = returnLocalTime(endServerDate)
                 dataHtml = dataHtml + ` <tr>
                 <td><div class="d-flex align-items-center">
           <img
@@ -43,8 +45,8 @@ $(window).on('load',()=>{
                 <td>${cls.description}</td>
                 <td class="p">${dayOfWeek[startDate.getDay()]} ${monthOfYear[startDate.getMonth()]} ${startDate.getDate()} ${startDate.getFullYear()} ${startDate.getHours()>12? startDate.getHours()-12:startDate.getHours()}:${startDate.getMinutes()} ${startDate.getHours() > 12? "PM":"AM"}</td>
                 <td>${dayOfWeek[endDate.getDay()]} ${monthOfYear[endDate.getMonth()]} ${endDate.getDate()} ${endDate.getFullYear()} ${endDate.getHours()>12? endDate.getHours()-12:endDate.getHours()}:${endDate.getMinutes()} ${endDate.getHours() > 12? "PM":"AM"}</td>
-                <td>${cls.end >  getCurrent() ?  "<span class='badge text-success  d-inline'><span class='bg-success mx-1 rounded-circle d-inline-block' style='width: 6px; height: 6px' ></span>Active</span>": "<span class='badge text-danger'><span class='bg-danger mx-1 rounded-circle d-inline-block' style='width: 6px; height: 6px' ></span>Past</span>" }</td>
-                <td><a class="btn btn-sm ${cls.end >  getCurrent() ?  "btn-success": "disabled btn-secondary"}" href="${cls.room_url}" ><i class="fa fa-sign-in"></i> Enter class</a></td>
+                <td>${endDate > new Date() ?  "<span class='badge text-success  d-inline'><span class='bg-success mx-1 rounded-circle d-inline-block' style='width: 6px; height: 6px' ></span>Active</span>": "<span class='badge text-danger'><span class='bg-danger mx-1 rounded-circle d-inline-block' style='width: 6px; height: 6px' ></span>Past</span>" }</td>
+                <td><a class="btn btn-sm ${endDate >  new Date() ?  "btn-success": "disabled btn-secondary"}" href="${cls.room_url}" ><i class="fa fa-sign-in"></i> Enter class</a></td>
               </tr>`
             });
             dataHtml += `</tbody></table>
@@ -53,9 +55,13 @@ $(window).on('load',()=>{
             $('#anu_class').DataTable();
         });
 
-        function getCurrent(){
-            let DateString = new Date()
-            return DateString.toISOString();
-        }
-})
+      })
+      
+function getCurrent(){
+    let DateString = new Date()
+    return DateString.toISOString();
+}
 
+function returnLocalTime(dateData) {
+  return new Date(dateData.getTime() + dateData.getTimezoneOffset() * 60000)
+}

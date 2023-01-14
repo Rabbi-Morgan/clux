22b3a1d4cef60f6c43750a5d0844f837a189daff
Anuoluwapo Omobolaji <francismobolaji1990@gmail.com>
2023-01-14 02:44:48 +0100

    new tab

--- public/js/homeclasses.js
+++ public/js/homeclasses.js
@@ -46,7 +46,7 @@ $(window).on('load',()=>{
                 <td class="p">${dayOfWeek[startDate.getDay()]} ${monthOfYear[startDate.getMonth()]} ${startDate.getDate()} ${startDate.getFullYear()} ${startDate.getHours()>12? startDate.getHours()-12:startDate.getHours()}:${startDate.getMinutes()} ${startDate.getHours() > 12? "PM":"AM"}</td>
                 <td>${dayOfWeek[endDate.getDay()]} ${monthOfYear[endDate.getMonth()]} ${endDate.getDate()} ${endDate.getFullYear()} ${endDate.getHours()>12? endDate.getHours()-12:endDate.getHours()}:${endDate.getMinutes()} ${endDate.getHours() > 12? "PM":"AM"}</td>
                 <td>${endDate > new Date() ?  "<span class='badge text-success  d-inline'><span class='bg-success mx-1 rounded-circle d-inline-block' style='width: 6px; height: 6px' ></span>Active</span>": "<span class='badge text-danger'><span class='bg-danger mx-1 rounded-circle d-inline-block' style='width: 6px; height: 6px' ></span>Past</span>" }</td>
-                <td><a class="btn btn-sm ${endDate >  new Date() ?  "btn-success": "disabled btn-secondary"}" href="${cls.room_url}" ><i class="fa fa-sign-in"></i> Enter class</a></td>
+                <td><a class="btn btn-sm ${endDate >  new Date() ?  "btn-success": "disabled btn-secondary"}" target="_blank" href="${cls.room_url}" ><i class="fa fa-sign-in"></i> Enter class</a></td>
               </tr>`
             });
             dataHtml += `</tbody></table>
